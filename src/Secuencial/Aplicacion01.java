f965bf780b99c239a1c20719ffd46a5e144438b1
EdderCodarlupo <edder.codarlupo@gmail.com>
2024-12-16 11:58:40 -0500

    Realice el ejercicio 1 en consola

--- src/Secuencial/Aplicacion01.java
+++ src/Secuencial/Aplicacion01.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Secuencial;
 
+import java.util.Scanner;
+
 /**
- *
- * @author edcom
+ * Crear una aplicación que solicite 
+ * ingresar dos números enteros y 
+ * permita realizar las operaciones matemáticas básicas con dichos números.
+ * SUMA, RESTA, MULTIPLICACIÓN, DIVISIÓN. RESTO, POTENCIA
  */
+
+    
 public class Aplicacion01 {
+    
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        // Declaracion de variables
+        int numero1, numero2, suma, resta, multiplicacion, division, resto, potencia;
+        System.out.println("Número 1: ");
+        numero1 = teclado.nextInt();
+        System.out.println("Número 2: ");
+        numero2 = teclado.nextInt();
+        // Proceso
+        suma = numero1 + numero2;
+        resta = numero1 - numero2;
+        multiplicacion = numero1 * numero2;
+        division = numero1 / numero2;
+        resto = numero1 % numero2;
+        potencia = (int) Math.pow(numero1, numero2);
+        // Salida
+        System.out.println("Resultados: \n"
+                + "Suma: " + suma + "\n"
+                + "Resta: " + resta + "\n"
+                + "Multiplicación: " + multiplicacion + "\n"
+                + "División: " + division + "\n"
+                + "Resto: " + resto + "\n"
+                + "Potencia: " + potencia + "\n"
+                );
+    
+    }
     
 }
